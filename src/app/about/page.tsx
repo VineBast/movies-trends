3f0be61fd978cdd4c427e2bd812f7ecf0346e9bb
Vine <vinebast@MacBook-Air-de-Vine.local>
2024-01-10 21:30:47 +0100

    corrected my english

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,9 +9,9 @@ const About = () => {
       <div className="mockup-code">
         <pre data-prefix="~" className="text-info"><code>About this project :</code></pre>
         <pre data-prefix="~"><code>To resume the loading text, movie trends is a web application from a scrapping project</code></pre>
-        <pre data-prefix="~"><code>proposing the real trend of US cinema releases of the week, scrapping different website specializing in cinema.</code></pre>
-        <pre data-prefix="~"><code>I'm also using the TMDB free API for precising data and to recover images.</code></pre>
-        <pre data-prefix="~"  className="text-warning"><code>This project is academic and has no commercial vocation.</code></pre>
+        <pre data-prefix="~"><code>showcasing the real trend of US cinema releases of the week, scrapping different websites dedicated to cinema.</code></pre>
+        <pre data-prefix="~"><code>I'm also using the TMDB free API for specific data and also to recover images.</code></pre>
+        <pre data-prefix="~"  className="text-warning"><code>This is an academic project, it has no commercial vocation.</code></pre>
       </div>
     </div>
   )
